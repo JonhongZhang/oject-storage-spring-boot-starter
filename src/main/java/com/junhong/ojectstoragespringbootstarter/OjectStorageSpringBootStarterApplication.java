3753406845b93d1f6300a11d396fcf162635cea6
zhangjunhong <904227036@qq.com>
2023-03-01 19:07:29 +0800

    Initial commit

---
+++ src/main/java/com/junhong/ojectstoragespringbootstarter/OjectStorageSpringBootStarterApplication.java
@@ -0,0 +1,15 @@
+package com.junhong.ojectstoragespringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjectStorageSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        char a = 47;
+        System.out.println(a + 2);
+
+        SpringApplication.run(OjectStorageSpringBootStarterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/junhong/ojectstoragespringbootstarter/OjectStorageSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhong.ojectstoragespringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjectStorageSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
